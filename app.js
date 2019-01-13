78f32cac5ccbe9887437cd3b54c5b9c4c0fa0597
TomaszWykpisz <t.wykpisz@gmail.com>
2019-01-13 20:05:37 +0100

    Learn some array methods

--- app.js
+++ app.js
@@ -56,3 +56,70 @@ console.log(add(2,5));
 console.log(add(3,5,7,1,2));
 
 ////
+// ARRAY
+
+var tab1 = [1,2,3,4];
+
+// Wybieranie elementu
+console.log(tab1[0]);
+
+// Długość tablicy
+console.log(tab1.length);
+
+// Dodawanie na koniec tablicy
+console.log(tab1.push(5, 6));
+
+// Usuwanie ostatniego elementu i zwracenie go   
+console.log(tab1.pop());
+
+// Dodawanie na początku tablicy
+console.log(tab1.unshift(0));
+
+// Uwuwanie pierwszego elementu i zwracanie go
+console.log(tab1.shift());
+
+// Łączenie tablicy w string z separatorem
+console.log(tab1.join(" - "));
+
+// Odwracanie tablicy 
+console.log(tab1.reverse());
+
+// Sortowanie po wartościach rosnąco
+console.log(tab1.sort());
+
+//Łączenie dwóch tablic
+var tab2 = [6, 7];
+// [] + [] = string
+console.log(tab1 + tab2);
+console.log(tab1.concat(tab2));
+
+// Wycinanie elementów (start, stop)
+console.log(tab1.slice(1,4));
+
+// Usuwanie / wstawianie elementów (start, ileUsunąć, noweELementy)
+console.log(tab1.splice(4,1));
+console.log(tab1);
+console.log(tab1.splice(4,0,5,6));
+console.log(tab1);
+
+// Wyszukiwanie indexu podanego elementu
+console.log(tab1.indexOf(3));
+
+// forEach()
+tab1.forEach( (el) => {
+	console.log(el);
+});
+
+// Sprawdzam czy wszystkie elementy spełniają warunek
+console.log(tab1.every((el)=>{ return el % 2 === 0 }));
+
+// Sprawdzam czy dowolny element spełnia warunek
+console.log(tab1.some((el)=>{ return el % 2 === 0 }));
+
+// map tworzy nową tablice o długości poprzedniej i okreśnoną funkcja
+console.log(tab1.map((el)=>{ return el * 2 }));
+
+// Filtrowanie 
+console.log(tab1.filter((el)=>{ return el % 2 === 0 }));
+
+////
